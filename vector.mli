057b31062f2ea0cca760d4e092c337095fa6cbf0
rafal09pl <rafal.lub.pl@gmail.com>
2017-01-16 21:44:22 +0100

    doc

--- vector.mli
+++ vector.mli
@@ -1,9 +1,10 @@
 val dot : float list -> float list -> float;;
-(* iloczyn skalarny*)
+(* scalar product*)
 
 val scale : float -> float list -> float list;;
-(*mnozenie przez skalar*)
+(* mulitiplication by scalar*)
 
 val sum : float list -> float list -> float list;;
 
 val vec_zero : int->float list;;
+(* zero vector of given size *)
